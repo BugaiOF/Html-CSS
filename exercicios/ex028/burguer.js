f19821d5d4e4c2c37b3f642f135d4a1f843b9dd2
Gustavo Da Silva Silveira <112825271+BugaiOF@users.noreply.github.com>
2022-12-21 19:47:30 -0300

    tamo indo

--- exercicios/ex028/burguer.js
+++ exercicios/ex028/burguer.js
@@ -9,3 +9,11 @@ function clickmenu(){
 
    }
 }
+
+function mudouTamanho(){
+   if (window.innerWidth >= 768)
+      menu.style.display= 'block'
+   else {
+      menu.style.display = 'none'
+   }
+} 
